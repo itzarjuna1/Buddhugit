89d2822e6399faaf91a80f061a2bad718b69670c
𝐊ʀɪsʜ 𝐌ɪsʜʀᴀ <163736172+Xkrishmishra@users.noreply.github.com>
2025-05-01 09:10:13 +0530

    Cᴏᴅᴇʀ~ᴋʀɪsʜ

--- TanuMusic/plugins/bot/mustjoin.py
+++ TanuMusic/plugins/bot/mustjoin.py
@@ -47,7 +47,7 @@ async def must_join_channel(app: Client, msg: Message):
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
-                                InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/krishsupport"),
+                                InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/KrishNetWork"),
                                 InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇ", url="https://t.me/Krishsupport"),
                             ]
                         ]
